7138b155e49a81e552387498a2869537bdc2a4a1
Jonathan Blake <jonathan.blake@quinnipiac.edu>
2022-09-19 09:45:29 -0400

    Next commits

--- Embedded-Examples/PlatformIO-Projects/Seeeduino_Xiao_Analog/src/main.cpp
+++ Embedded-Examples/PlatformIO-Projects/Seeeduino_Xiao_Analog/src/main.cpp
@@ -2,9 +2,10 @@
 
 // the setup routine runs once when you press reset:
 void setup() {
+  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-}
+  pinMode(LED_BUILTIN, OUTPUT);
 
 // the loop routine runs over and over again forever:
 void loop() {
