339ae9f3e6e60caa46232f67a25278165048c81e
Jonathan Blake <jonathan.blake@quinnipiac.edu>
2022-10-19 12:54:43 -0400

    Latest

--- Embedded-Examples/Arduino-Projects/Firebeetle_2_Empty_Dual/Firebeetle_2_Empty_Dual.ino
+++ Embedded-Examples/Arduino-Projects/Firebeetle_2_Empty_Dual/Firebeetle_2_Empty_Dual.ino
@@ -3,6 +3,7 @@ TaskHandle_t Task1;
 void Task1code(void *pvParameters ){
 
   while (1) {
+    delay(100);
   }
 }
 
@@ -10,7 +11,7 @@ void Task1code(void *pvParameters ){
 void setup() {
   Serial.begin(115200);
 
-  xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,0,&Task1,0);           
+  xTaskCreatePinnedToCore(Task1code,"Task1",10000,NULL,1,&Task1,0);           
   delay(500);
 }
 
